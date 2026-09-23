99dae8f5b74d5719b87522d849fd5d9f0dda3806
ppunyaph <68140120@kmitl.ac.th>
2026-09-23 17:18:36 +0700

    s

--- cell01/ex00/name.py
+++ cell01/ex00/name.py
@@ -1,3 +1,3 @@
 first_name = "Panita"
 last_name = "Punyaphonmonkol"
-print(first_name , last_name)
+print(first_name + " " + last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Panita"
+last_name = "Punyaphonmonkol"
+whole_name = first_name + " " + last_name
+print(whole_name)
